fcf359eebfa079f46c2f2f868a467c86c0f3052b
Miroslav Křenek <krenekmiroslav@gmail.com>
2019-01-29 11:58:24 +0100

    improved plants generating
    Plants are now cleared when regenerated.
    Added Clear Plants button.
    Clearing plants using plants pool for re-generating for optimal performance.

--- Assets/Scripts/Plants.cs
+++ Assets/Scripts/Plants.cs
@@ -1,57 +1,120 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Profiling;
 
 public class Plants : MonoBehaviour
 {
-    [SerializeField] [Range(100, 10000)] int numberOfPlants = 100;
+    [SerializeField] [Range(100, 10000)] int maxNumberOfPlants = 100;
     [SerializeField] float plantSize = 1f;
     [SerializeField] float terrainSize = 100f;
+
     [SerializeField] GameObject plantPrefab;
     [SerializeField] LayerMask terrainMask;
-    Transform plantsParent;
+    [SerializeField] Transform plantsOnTerrainParent;
+    [SerializeField] Transform plantsPoolParent;
+
+    List<GameObject> plantsOnTerrain = new List<GameObject>();
+    List<GameObject> plantsPool = new List<GameObject>();
+
+    Vector3 plantsPoolPosition = new Vector3(1000, 1000, 1000);
 
     private void Start()
     {
-        plantsParent = GetComponent<Transform>();
-    }
-
-
-    public void GeneratePlants()
-    {
-        if (plantPrefab != null)
-        {
-            for (int i = 0; i < numberOfPlants; i++)
-            {
-                GameObject newPlant = InstantiateNewPlant();
-                MovePlantToSurface(newPlant);
-
-                // add plant to list of plant on terrain
-
-
-            }
-        }
-        else
+        if (plantPrefab == null)
         {
             Debug.LogError("Plant prefab is missing!");
         }
+    }
+
+    public void GeneratePlants()
+    {
+        int plantsToBeGenerated = maxNumberOfPlants;
+
+        if (plantsOnTerrain.Count > 0)
+        {
+            ClearPlants();
+        }
+
+        MovePlantsFromPool(ref plantsToBeGenerated);
+
+        if (plantsToBeGenerated > 0)
+        {
+            for (int i = 0; i < plantsToBeGenerated; i++)
+            {
+                InstantiateNewPlant();
+            }
+        }
+    }
+
+
+    /*public void GeneratePlantsWithDestroy()
+    {
+        DestroyAllPlants();
+        for (int i = 0; i < maxNumberOfPlants; i++)
+        {
+            InstantiateNewPlant();
+        }
 
     }
 
-    private void MovePlantToSurface(GameObject newPlant)
+    private void DestroyAllPlants()
+    {
+        for (int i = 0; i < plantsOnTerrain.Count; i++)
+        {
+            Destroy(plantsOnTerrain[i]);
+        }
+    }
+    */
+
+    private void MovePlantsFromPool(ref int plantsToBeGenerated)
+    {
+        while (plantsPool.Count > 0 && plantsToBeGenerated > 0)
+        {
+            MovePlant(plantsPool[0], PlantRandomPosition(), plantsOnTerrainParent);
+            plantsOnTerrain.Add(plantsPool[0]);
+            plantsPool.RemoveAt(0);
+            plantsToBeGenerated--;
+        }
+    }
+
+    public void ClearPlants()
+    {
+        for (int i = 0; i < plantsOnTerrain.Count; i++)
+        {
+            MovePlant(plantsOnTerrain[i], plantsPoolPosition, plantsPoolParent);
+        }
+        plantsPool.AddRange(plantsOnTerrain);
+        plantsOnTerrain.Clear();
+    }
+
+    private void InstantiateNewPlant()
+    {
+        GameObject newPlant = Instantiate(plantPrefab, PlantRandomPosition(), Quaternion.identity, plantsOnTerrainParent);
+        plantsOnTerrain.Add(newPlant);
+        newPlant.transform.SetParent(plantsOnTerrainParent);
+    }
+
+
+    private Vector3 PlantRandomPosition()
     {
         RaycastHit terrainSurface;
-        Physics.Raycast(newPlant.transform.position, Vector3.down, out terrainSurface, 1000, terrainMask);
-        newPlant.transform.position = new Vector3(newPlant.transform.position.x, terrainSurface.point.y + (plantSize / 2), newPlant.transform.position.z);
+
+        Vector3 newPlantPosition = new Vector3(
+            UnityEngine.Random.Range(plantSize / 2, terrainSize - (plantSize / 2)),
+            100f,
+            UnityEngine.Random.Range(plantSize / 2, terrainSize - (plantSize / 2))
+            );
+        Physics.Raycast(newPlantPosition, Vector3.down, out terrainSurface, 1000, terrainMask);
+        newPlantPosition.y = terrainSurface.point.y + (plantSize / 2);
+
+        return newPlantPosition;
     }
 
-    private GameObject InstantiateNewPlant()
+    private void MovePlant(GameObject plant, Vector3 newPosition, Transform newParent)
     {
-        Vector3 newPlantPosition = new Vector3(
-            Random.Range(plantSize / 2, terrainSize - (plantSize / 2)),
-            100f,
-            Random.Range(plantSize / 2, terrainSize - (plantSize / 2))
-            );
-        return Instantiate(plantPrefab, newPlantPosition, Quaternion.identity, plantsParent);
+        plant.transform.position = newPosition;
+        plant.transform.SetParent(newParent);
     }
 }
